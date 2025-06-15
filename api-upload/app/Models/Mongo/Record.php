92730ca8e24c3f612937e7904a0b49a8b5883bea
Vinicius Vitorino <vitorino.developer@outlook.com>
2025-06-15 04:00:38 -0300

    ✅ feat: adiciona model Record com conexão MongoDB e integração com Repository Pattern

---
+++ api-upload/app/Models/Mongo/Record.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\Mongo;
+
+use MongoDB\Laravel\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+class Record extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    protected $connection = 'mongodb';
+    protected $collection = 'records';
+
+    protected $fillable = [
+        'upload_id',
+        'RptDt',
+        'TckrSymb',
+        'MktNm',
+        'SctyCtgyNm',
+        'ISIN',
+        'CrpnNm'
+    ];
+}
+
