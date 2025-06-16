063bffecf9acf242fa0687e0fe11a448fe12b2a2
Vinicius Vitorino <vitorino.developer@outlook.com>
2025-06-16 04:41:34 -0300

    ✨ feat: Entidade Upload utilizada com MySQL

--- api-upload/app/Models/Upload.php
+++ api-upload/app/Models/Upload.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\Activitylog\LogOptions;
 
 /**
  * Class Upload.
@@ -13,7 +15,7 @@ use Prettus\Repository\Traits\TransformableTrait;
  */
 class Upload extends Model implements Transformable
 {
-    use TransformableTrait;
+    use TransformableTrait, LogsActivity;
 
     /**
      * The attributes that are mass assignable.
@@ -23,7 +25,17 @@ class Upload extends Model implements Transformable
     protected $fillable = [
         'uuid',
         'file_name',
+        'file_path',
         'rpt_dt',
     ];
 
+    /**
+     * Get the options for activity logging.
+     *
+     * @return \Spatie\Activitylog\LogOptions
+     */
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults();
+    }
 }
